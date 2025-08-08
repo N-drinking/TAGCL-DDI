75e3e901e685f8420b067c5926b12295c291e4f4
N-drinking <dhge@qfnu.edu.cn>
2025-08-09 00:15:02 +0800

    Update 5timesrun.py

--- TAGCL-DDI-main/codes for MRCGNN/5timesrun.py
+++ TAGCL-DDI-main/codes for MRCGNN/5timesrun.py
@@ -25,15 +25,4 @@ for k in range(5):
     os.system('python main_test.py --out_file test.txt --zhongzi '+str(k))
 
 
-# loss_ncla_range = [0.4, 0.5, 0.6, 0.7,1]  # 学习率的范围，可以根据需要调整
-#
-# for loss_ncla in loss_ncla_range:
-#      os.system('python main.py --out_file test.txt --loss_ncla ' + str(loss_ncla) + ' --zhongzi '+str(0))
-# loss_autoGCL_range = [0.0005, 0.005, 0.05, 0.5,1]  # 学习率的范围，可以根据需要调整
-#
-# for loss_autoGCL in loss_autoGCL_range:
-#      os.system('python main.py --out_file test.txt --loss_autoGCL ' + str(loss_autoGCL) + ' --zhongzi '+str(0))
-
-
-
 
